47f2c01de96b488e097ce62d4042503b0331dd55
Lilyfostergrellis <lilyfostergrellis2@gmail.com>
2022-12-15 16:03:57 +0000

    deleted instructional readme file

--- index.js
+++ index.js
@@ -107,6 +107,43 @@ for (var value of profits) {
 
 console.log("Total: $" + SumOfProfit);
 
+// 3. The average of the changes in Profit/Losses over the entire period. You will need to track what the total change in profits are from month to month and then find the average. (Total/Number of months).
+
+// I already found the Sum of Profit and total Months above. I used the Total Profit / Number of months formula here.
+var averageChange = SumOfProfit / totalMonths;
+
+// I used this formula to reduce the decimal extension to 2 digits.
+averageChange = Number(averageChange.toFixed(2));
+
+console.log("Average Change: $" + averageChange);
+
+
+// 4. The greatest increase in profits (date and amount) over the entire period.
+
+// I used the function below to sort the profits from higher to lower.
+var higherToLower = monthlyProfits.sort(function(a, b){return b - a});
+// I took the index 0 because it will be the highest Profit.
+var higherProfit = higherToLower[0];
+// I also used a find feature because the month was also requested. Therefore, I used the highest Profit to find the month.
+var findHigherProfitMonth = finances.find(month => month[1] == higherProfit);
+
+var HigherProfitMonth = findHigherProfitMonth[0];
+
+console.log("Greatest Increase in Profits: "+ HigherProfitMonth + ": $" + higherProfit);
+
+
+// 5. The greatest decrease in losses (date and amount) over the entire period.
+// I did the same operations above again here, Only, I changed the function from lower to higher.
+var lowerToHigher = monthlyProfits.sort(function(a, b){return a - b});
+
+var lowerProfit = lowerToHigher[0];
+
+var findLowerProfitMonth = finances.find(month => month[1] == lowerProfit);
+
+var lowerProfitMonth = findLowerProfitMonth[0];
+
+console.log("Greatest Decrease in Profits: " + lowerProfitMonth + ": $" + lowerProfit);
+
 // The net total amount of Profits/Losses over the entire period.
  
 
